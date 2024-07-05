be255f1276c94ad7de92d6f273a442e61431043a
Adarsh <adarsh.bhardwaj.w@gmail.com>
2024-07-05 15:31:30 +0530

    first

--- utils/api.ts
+++ utils/api.ts
@@ -1,21 +1,24 @@
+'use client'
 export async function fetchChatResponse(prompt: String, model: String) {
     const apiUrl =
-      "https://openai-endpoints-60026948054.development.catalystserverless.in/server/openai_endpoints_function/chat";
+      "https://digismiths-api-60026948054.development.catalystserverless.in/server/digismiths_api_function/";
+    // const apiUrl = "https://api.restful-api.dev/objects";
   
     try {
       const response = await fetch(apiUrl, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ prompt, model }),
+        method: "GET",
+        // headers: {
+        //   "Content-Type": "application/json",
+        //   "Accept":"*/*",
+        // },
+        // body: JSON.stringify({ prompt, model }),
       });
   
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
+      // if (!response.ok) {
+      //   throw new Error("Network response was not ok");
+      // }
   
-      return response.json();
+      return response;
     } catch (error) {
       console.error("Error fetching chat response:", error);
       throw new Error("Error fetching chat response");
