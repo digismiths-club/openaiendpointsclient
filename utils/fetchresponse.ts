5dc94e64dfd5471030cab920fd72bb90279f0e55
Adarsh <adarsh.bhardwaj.w@gmail.com>
2024-07-06 19:39:01 +0530

    api working

--- utils/fetchresponse.ts
+++ utils/fetchresponse.ts
@@ -1,26 +1,26 @@
-'use client'
-export async function fetchChatResponse(prompt: String, model: String) {
-    const apiUrl =
-      "https://digismiths-api-60026948054.development.catalystserverless.in/server/digismiths_api_function/";
-    // const apiUrl = "https://api.restful-api.dev/objects";
+// 'use client'
+// export async function fetchChatResponse(prompt: String, model: String) {
+//     const apiUrl =
+//       "https://digismiths-api-60026948054.development.catalystserverless.in/server/digismiths_api_function/";
+//     // const apiUrl = "https://api.restful-api.dev/objects";
   
-    try {
-      const response = await fetch(apiUrl, {
-        method: "GET",
-        // headers: {
-        //   "Content-Type": "application/json",
-        //   "Accept":"*/*",
-        // },
-        // body: JSON.stringify({ prompt, model }),
-      });
+//     try {
+//       const response = await fetch(apiUrl, {
+//         method: "GET",
+//         // headers: {
+//         //   "Content-Type": "application/json",
+//         //   "Accept":"*/*",
+//         // },
+//         // body: JSON.stringify({ prompt, model }),
+//       });
   
-      // if (!response.ok) {
-      //   throw new Error("Network response was not ok");
-      // }
+//       // if (!response.ok) {
+//       //   throw new Error("Network response was not ok");
+//       // }
   
-      return response;
-    } catch (error) {
-      console.error("Error fetching chat response:", error);
-      throw new Error("Error fetching chat response");
-    }
-  }
+//       return response;
+//     } catch (error) {
+//       console.error("Error fetching chat response:", error);
+//       throw new Error("Error fetching chat response");
+//     }
+//   }
